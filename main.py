862f1c551bbb940b3b7030fd8dd8ed39cf916fc8
Aleksander Knypek <Aleksander.Knypek@ncbj.gov.pl>
2024-03-26 11:06:49 +0100

    Display

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 print("Git Handson")
+DATA = [
+    {
+        "imie": "Kacper",
+        "gatunek": "Homo sapiens",
+        "waga": 90,
+        "wiek": 19.5
+    },
+    {
+        "imie": "Uszatek",
+        "gatunek": "mis",
+        "waga": 40,
+        "wiek": 60
+    },
+    {
+        "imie": "Reksio",
+        "gatunek": "pies",
+        "waga": 6,
+        "wiek": 10,
+    },
+]
+Dane = DATA
+def display(Dane):
+    print("Dane:", Dane)
+display(Dane)
